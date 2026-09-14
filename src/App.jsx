eca5990d98bdeb84cbbfb4f4eecb8343b49a0fe0
Tahereh Faraji <taherehfaraji100@gmail.com>
2026-09-14 20:15:21 +0330

    todo-reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,65 @@
-import { useState } from 'react';
-import Alert from './Alert';
-import './App.css';
-// import AutoResetCounter from './AutoResetCounter';
-// import RandomUsers from './RandomUsers';
-// import TypingIndicator from './TypingIndicator';
-// import FocusInput from './FocusInput';
-// import GitHubFinder from './GitHubFinder';
-// import Posts from './Posts';
-// import Component from './Component';
-// import Video from './Video';
-import Counter from './Counter'
-import ThemeSwitcher from './ThemeSwitcher'
-import SignUp from './SignUp'
+// import { useState } from 'react';
+// import Alert from './Alert';
+// import './App.css';
+// // import AutoResetCounter from './AutoResetCounter';
+// // import RandomUsers from './RandomUsers';
+// // import TypingIndicator from './TypingIndicator';
+// // import FocusInput from './FocusInput';
+// // import GitHubFinder from './GitHubFinder';
+// // import Posts from './Posts';
+// // import Component from './Component';
+// // import Video from './Video';
+// import Counter from './Counter'
+// import ThemeSwitcher from './ThemeSwitcher'
+// import SignUp from './SignUp'
 
-function App() {
-  const [showAlert, setShowAlert] = useState(null);
+// // function App() {
+// //   const [showAlert, setShowAlert] = useState(null);
 
-  return (
-    <>
-      {/* <div className='container'>
-        <button onClick={() => { setShowAlert("success") }}>Success Alert</button>
-        {showAlert === "success" && (
-          <Alert type="success"
-            title="موفق شدید!"
-            message="عملیات با موفقیت انجام شد"
-            onClose={() => { setShowAlert(null) }} />
-        )}
+// //   return (
+// //     <>
+//       {/* <div className='container'>
+//         <button onClick={() => { setShowAlert("success") }}>Success Alert</button>
+//         {showAlert === "success" && (
+//           <Alert type="success"
+//             title="موفق شدید!"
+//             message="عملیات با موفقیت انجام شد"
+//             onClose={() => { setShowAlert(null) }} />
+//         )}
 
-        <button onClick={() => { setShowAlert("error") }}>Error Alert</button>
-        {showAlert === "error" && (
-          <Alert type="error"
-            title="خطا!"
-            message="خطایی رخ داد.لطفا دوباره تلاش کنید"
-            onClose={() => { setShowAlert(null) }} />
-        )}
+//         <button onClick={() => { setShowAlert("error") }}>Error Alert</button>
+//         {showAlert === "error" && (
+//           <Alert type="error"
+//             title="خطا!"
+//             message="خطایی رخ داد.لطفا دوباره تلاش کنید"
+//             onClose={() => { setShowAlert(null) }} />
+//         )}
 
-        <button onClick={() => { setShowAlert("warning") }}>Warning Alert</button>
-        {showAlert === "warning" && (
-          <Alert type="warning"
-            title="هشدار!"
-            message="لطفاً اطلاعات واردشده را بررسی کنید"
-            onClose={() => { setShowAlert(null) }} />
-        )}
-      </div> */}
-       {/* <AutoResetCounter />
-      <RandomUsers /> 
-       <TypingIndicator />
-      <FocusInput /> */}
-      {/* <GitHubFinder /> */}
-      {/* <Posts />
-      <Component /> */}
-      {/* <Video /> */}
-      <Counter />
-      <ThemeSwitcher />
-      <SignUp />
-    </>
-  )
+//         <button onClick={() => { setShowAlert("warning") }}>Warning Alert</button>
+//         {showAlert === "warning" && (
+//           <Alert type="warning"
+//             title="هشدار!"
+//             message="لطفاً اطلاعات واردشده را بررسی کنید"
+//             onClose={() => { setShowAlert(null) }} />
+//         )}
+//       </div> */}
+//        {/* <AutoResetCounter />
+//       <RandomUsers /> 
+//        <TypingIndicator />
+//       <FocusInput /> */}
+//       {/* <GitHubFinder /> */}
+//       {/* <Posts />
+//       <Component /> */}
+//       {/* <Video /> */}
+//       {/* <Counter />
+//       <ThemeSwitcher />
+//       <SignUp />
+//     </>
+//   )
 
-}
+// }
 
-export default App
+// export default App */}
 
 //////////////////////////////
 // import { useState } from 'react'
@@ -123,3 +123,20 @@ export default App
 // }
 
 // export default App
+
+import TodoProvider from './TodoProvider'
+import AddTodo from './AddTodo'
+import TodoList from './TodoList'
+import TodoFooter from './TodoFooter'
+
+function App() {
+  return (
+    <TodoProvider>
+      <h1>Todo with Context + Reducer</h1>
+      <AddTodo />
+      <TodoList />
+      <TodoFooter />
+    </TodoProvider>
+  )
+}
+export default App
